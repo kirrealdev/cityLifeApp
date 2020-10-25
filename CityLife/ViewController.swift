99ff4e66e3c1a95bfb5963e4f7a0b2f0f59b8eef
KirRealDev <mkir5@yandex.ru>
2020-10-25 16:21:14 +0300

    Initial Commit

---
+++ CityLife/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CityLife
+//
+//  Created by KirRealDev on 25.10.2020.
+//  Copyright © 2020 KirRealDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
